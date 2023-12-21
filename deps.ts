ebce3c81dca073abb4de6864abde0ca4d7d59540
GitHub <noreply@github.com>
2023-12-21 19:44:39 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { mergeReadableStreams } from "https://deno.land/std@0.209.0/streams/mod.ts";
+export { mergeReadableStreams } from "https://deno.land/std@0.210.0/streams/mod.ts";
 export { router } from "https://deno.land/x/rutt@0.2.0/mod.ts";
